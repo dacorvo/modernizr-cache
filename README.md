a168faea4a23d98c99d4179536a48b51b852c481
David Corvoysier <kaizouman@kaizou.org>
2012-12-04 19:36:17 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,9 @@
+# modernizr-cache
+
+A simple PHP script that allows [Modernizr](http://modernizr.com) feature detection code to be run only once in a browsing session.
+
+Usage: Just include the modernizr-cache.php file as follows:
+
+	<script type="text/javascript" src="./modernizr-cache.php"></script>
+    
+
---
+++ modernizr-cache.php
@@ -0,0 +1,55 @@
+<?php
+    static $modernizr_js = './Modernizr/modernizr.js';
+    static $features_dir = './Modernizr/feature-detects';
+    static $key = 'Modernizr';
+    
+    header('Content-type: text/javascript');
+
+    readfile($modernizr_js);
+?>
+(function(){
+<?php
+    if (isset($_COOKIE) && isset($_COOKIE[$key])) {
+      $cookie = $_COOKIE[$key];
+      $modernizr = (get_magic_quotes_gpc() ? stripslashes($cookie) : $cookie);
+?>
+    var cachedModernizr = <?php print $modernizr;?>;
+    for (var f in cachedModernizr) {
+        if(f[0]=='_'){
+            continue;
+        }
+        var t=typeof cachedModernizr[f];
+        if(t=='function'){
+            continue;
+        }
+        if(t=='object'){
+          for(var s in cachedModernizr[f]){
+            //"c+='/'+s+':'+(m[f][s]?'1':'0');".
+          }
+        }else{
+          window.Modernizr[f] = cachedModernizr[f];
+        }
+    }
+<?php
+    } else {
+      $features_path = dirname(__FILE__). '/' . $features_dir;
+      $files = scandir($features_path);
+?>
+    document.addEventListener('DOMContentLoaded',function(evt) {
+<?php
+      foreach($files as $file){
+          readfile($features_path . '/' . $file);
+      }
+?>
+        var c='<?php print $key;?>='+JSON.stringify(Modernizr);
+        try{
+            document.cookie=c;
+        }catch(e){}
+    },
+    false);
+
+<?php
+    }
+?>
+
+})();
